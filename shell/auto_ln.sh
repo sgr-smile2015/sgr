88bf6d89dce8c535834fc74350bae1706363aef2
sgr <sgr@sss.com>
2016-08-30 11:23:52 +0800

    修改auto_ln.sh文件，添加修改版本号功能

--- shell/auto_ln.sh
+++ shell/auto_ln.sh
@@ -1,26 +1,53 @@
 #! /bin/bash
 # the scripts is usefull auto link to something
 #author by lol
-		
+
+#截取配置文件中version的值
+ver=`cat ../apps_conf/release/config.properties_2016-05-17 |grep "version=" |awk -F = '{print $2}'`
+#num=`echo $ver |xargs awk -F = '{print $2}' `
+#去掉中间的 .
+num=`echo $ver | tr -d '.'`
+#自＋1 ，更新version版本
+num=`expr $num + 1`
+#new=`echo $num |sed -r 's/^()$/\1./g' `
+#更新版本后填入. -> 888 变成 8.8.8.
+new=`echo $num |sed 's/./&./g'`
+#去掉最后的.
+new=${new%?}
+
+sed -i "s/${ver}/${new}/g" ../apps_conf/release/config.properties_2016-05-17
 #find the locat dir link file such as test.war
-LN_FILE=`find -type l |awk -F / '{print $2}'` 
+LN_FILE=`find -type l |awk -F / '{print $2}'`
 
 #if the link file use .war end , rm the test dir
 FLAG=`echo $LN_FILE |awk -F . '{print $2}'`
 DIR_WAR=`echo $LN_FILE |awk -F . '{print $1}'`
 
-if [ ${FLAG} = 'war' ] 
-	then
-	echo "rm ${DIR_WAR}"
-	rm -r ${DIR_WAR}
-	fi
+if [ ${FLAG} = 'war' ]
+    then
+    echo "stop the server--> $DIR_WAR"
+    ../bin/shutdown.sh 
+    echo "Delete --> ${DIR_WAR}"
+    rm -r ${DIR_WAR}
+else
+    ../bin/server.sh stop
+    fi
 
 
 #LA_FILE=`find  test/ -type f -mmin -2 |awk -F / '{print $2}'` 
-LA_FILE=`find  release/ -type f -mmin -1` 
+LA_FILE=`find  release/ -type f -mmin -30`
 
 if [ ! -z $LA_FILE ]
-	then 
-	echo "link ${LA_FILE} --> ${LN_FILE}"
-	ln -sf ${LA_FILE} ${LN_FILE} 
+    then
+    echo "link ${LA_FILE} --> ${LN_FILE}"
+    ln -sf ${LA_FILE} ${LN_FILE}
 fi
+
+sleep 2s
+
+if [ ${FLAG} = 'war' ]
+    then
+    ../bin/start.sh 
+else
+    ../bin/server.sh start
+    fi
