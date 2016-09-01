a9520f5672e43c97d0b05803114e884d6ba28d6a
sgr <sgr@sss.com>
2016-09-01 16:17:22 +0800

    zabbix python scripts

---
+++ python/zabbix_py/check_mongodb.py
@@ -0,0 +1,58 @@
+#! /usr/bin/python
+#Auth:lol
+#function:check process_nameor check process_port
+#date:2016-8-81
+  
+import os
+import sys
+import commands
+  
+def help():
+	print "Usage:  "
+	print "       %s process_name [process_port]"%sys.argv[0]
+	print "Example: "
+	print "       %s mysql         ;If the process_nameexists, output 1, otherwise 0"%sys.argv[0]
+	print "       %s nginx  80     ;If the process_port exists, output 1,otherwise 0"%sys.argv[0]
+	print "       %s mysql  3306 "%sys.argv[0]
+  
+def check_process_name():
+	process_num = commands.getstatusoutput(" ps -ef|grep -v grep|grep -v %s | grep -v '\[%s\]'|grep %s|wc -l " %(sys.argv[0],sys.argv[1],sys.argv[1]))
+	#print process_num
+	if not process_num[1]:
+		print "0"
+		return
+	if int(process_num[1]) >= 1:
+		print "1"
+	else:
+		print "0"
+
+def check_process_port():
+	process_num=commands.getstatusoutput("netstat -lnt|grep -v grep|grep ':%s '|wc -l" %sys.argv[2])
+	if int(process_num[1]) >=1:
+		print "1"
+	else: 
+		print "0"
+  
+def check_mongodb_connect():
+	process_num=commands.getstatusoutput("mongostat -h 192.168.1.82 -u ipin -p xxoo --authenticationDatabase admin -n 1 | awk '{ if($16~/^([0-9])+$/) {print $16}}'")
+	print process_num
+	if int(process_num[1]) >=1:
+		print process_num
+	else:
+		print "fuck"
+
+###start execute
+print sys.argv
+
+if sys.argv[1] == 'mongodb':
+	check_mongodb_connect()
+	sys.exit()
+elif len(sys.argv) == 2:
+	check_process_name()
+	sys.exit()
+elif len(sys.argv) == 3:
+	check_process_port()
+	sys.exit()
+else:
+	help()
+	sys.exit()
---
+++ python/zabbix_py/sendmail.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+  
+import smtplib
+import sys
+from email.mime.text import    MIMEText
+from email.header import Header
+import time
+#reload(sys)
+#sys.setdefaultencoding('utf8')
+current_time=time.strftime('%Y-%m-%d %H:%M',time.localtime(time.time()))
+  
+mail_host ='smtp.exmail.qq.com'
+mail_user ='yangxx00@ipin.com'
+mail_pwd = 'xxo02016'
+  
+def send_email( content,mailto, get_sub ):
+    #msg = MIMEText( content.encode('utf8'),_subtype = 'html', _charset = 'utf8')
+    #msg = MIMEText(content,_subtype='plain',_charset='gb2312')
+    msg = MIMEText(content,_subtype='plain',_charset='utf-8')
+    msg['From'] =mail_user
+    msg['Subject'] =get_sub
+    msg['To'] = ",".join( mailto )
+    
+    try:
+        s = smtplib.SMTP_SSL( mail_host, 465)       
+        s.login(mail_user, mail_pwd )
+        s.sendmail(mail_user, mailto,msg.as_string())
+        s.close()
+    except Exception as e:
+        print 'Exception: ', e
+  
+title=sys.argv[2]
+cont="""
+---------------------------------
+摘要:  %s
+---------------------------------
+时间: %s
+---------------------------------
+  
+"""%(sys.argv[3],current_time)
+  
+  
+to_list = [
+         '%s'%(sys.argv[1]),
+        ]
+  
+with    open('/tmp/sendmail_qs.log','ab') as f:
+        f.write('%s  Receive address:  %s Title: %s \n'%(current_time,sys.argv[1],title))
+send_email( cont, to_list,title)
