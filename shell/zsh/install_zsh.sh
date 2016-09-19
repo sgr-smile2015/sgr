b4dc938c252f885089bfc69093c41e66984f0e3b
sgr <sgr@sss.com>
2016-09-19 10:30:44 +0800

    添加关于zshrc的配置文件以及，zsh的安装方式

--- shell/zsh/install_zsh.sh
+++ shell/zsh/install_zsh.sh
@@ -1,6 +1,11 @@
+#安装zsh，ubuntu 14.04使用apt安装
 sudo apt-get install zsh
+#安装 oh-my-zsh 
 sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 source .zshrc
 
-安装powerline
+#配置zshrc文件,使用 agnoster主题
+ZSH_THEME="agnoster"
+
+
 
