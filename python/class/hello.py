4dc96bd1bfc870ffbf715d3cfb8b6475abda992b
sgr <sgrqihai997@gmail.com>
2016-10-10 17:59:31 +0800

    fish hello.py scritps

--- python/class/hello.py
+++ python/class/hello.py
@@ -4,5 +4,7 @@ the is testing module file to python scripts
 """
 
 print "hello github.com"
-print "testing file" 
-print "testing file to my book story" 
+print "He said, \"i'm yours!\"" 
+print "\\\\_v_//" 
+print "Stay hungry,\nstay foolish,\n     -- Steve Jobs" 
+print "*\n***\n*****\n***\n*" 
