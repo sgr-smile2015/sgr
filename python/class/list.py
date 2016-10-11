0a0737329c0372caa759caf520a349179caa2f20
sgr <sgrqihai997@gmail.com>
2016-10-11 15:36:31 +0800

    list testing

--- python/class/list.py
+++ python/class/list.py
@@ -5,3 +5,12 @@ print range(1, 9)
 x = range(1,9)
 for i in x:
     print i,
+
+list = [32, 'everyday', 0.8,True]
+print 
+print list[1]
+list[3] = 98
+print list[3]
+
+list.append(234)
+print list[-1]
