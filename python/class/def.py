cdf1145a37ce1ee52d4cf0fe51d0f0fbfbd98f1b
sgr <sgrqihai997@gmail.com>
2016-10-11 15:21:14 +0800

    add def equal

--- python/class/def.py
+++ python/class/def.py
@@ -1,6 +1,29 @@
 #! /usr/bin/python
 
+from random import randint
+num = randint(1,12)
+
+def equal(num1,num2):
+    if num1 < num2:
+        print 'too small'
+        return False;
+    if num1 > num2:
+        print 'too big ^_^'
+        return False;
+    if num1 == num2:
+        print 'bingo !!'
+        return True
+
+
+print 'guess numbers "?"'
+bingo = False
+while bingo == False:
+    answer = input()
+    bingo = equal(answer, num)
+
 def hello(someone):
     print someone + ' say hello'
 
 hello('tom')
+
+
