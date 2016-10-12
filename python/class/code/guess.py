f310e66ce1004d42b121be72db40a6de0e6a1636
sgr <sgrqihai997@gmail.com>
2016-10-12 14:58:16 +0800

    use code to games

---
+++ python/class/code/guess.py
@@ -0,0 +1,19 @@
+#! /usr/bin/python
+
+from random import randint 
+num = randint(1,2000)
+
+print 'guess number ?'
+
+bingo = False
+while bingo == False:
+    answer = input()
+
+    if answer < num:
+        print 'too small'
+    if answer > num:
+        print 'too big'
+    if answer == num:
+        print 'bingo'
+        bingo = True
+
