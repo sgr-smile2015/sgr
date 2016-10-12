69384024b3c37eead4e8a99553d5cbcc67184f65
sgr <sgrqihai997@gmail.com>
2016-10-12 10:26:32 +0800

    add file split.py

---
+++ python/class/split.py
@@ -0,0 +1,14 @@
+#! /usr/bin/python
+
+test ='i am an english teacher' 
+it ='i am. an english. teacher' 
+
+print test.split()
+print it.split('.')
+
+s = '->'
+
+ip = ['apple', 'plus','S6']
+print s.join(ip)
+print '_'.join(ip)
+print ''.join(ip)
