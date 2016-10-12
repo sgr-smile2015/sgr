a220e0d1237592cddbaad9fcfaa9650d2fdc01c8
sgr <sgrqihai997@gmail.com>
2016-10-12 16:10:38 +0800

    bug xiufu

--- python/class/code/day1/guess.py
+++ python/class/code/day1/guess.py
@@ -1,23 +1,33 @@
 #! /usr/bin/python
 
 from random import randint 
+name = raw_input('you name->:')
 num = randint(1,10)
 
 f = open('game.txt')
-score = f.read().split()
+lines = f.readlines()
+f.close()
+
+scores = {}
+for l in lines:
+    s = l.split()
+    scores[s[0]] = s[1:]
+score = scores.get(name)
+if score is None:
+    score = [0,0,0]
 
 game_time = int(score[0])
 min_time = int(score[1])
 total_time = int(score[2])
 
 if game_time > 0:
-    avg_time = float (total_time) / game_time
+    avg_time = float(total_time) / game_time
 else:
     avg_time = 0
 
 #print 'play time %d s, min time %d s, avg time %0.2f s' % (game_time,min_time,total_time)
-print 'play time %d s, min time %d s, avg time %0.2f s' \
-% (game_time,min_time,total_time) 
+print '%s , play time %d s, min time %d s, avg time %0.2f s' \
+% (name,game_time,min_time,avg_time) 
 
 
 times = 0
@@ -41,7 +51,13 @@ if game_time == 0 or times < min_time:
 
 total_time += times
 game_time += 1
-result = '%d %d %d' %(game_time,min_time,total_time)
+scores[name] = [str(game_time),str(min_time),str(total_time)]
+
+result = '' 
+#result = '%d %d %d' %(game_time,min_time,total_time)
+for n in scores:
+    line = n + ' ' + ' '.join(scores[n]) + '\n'
+    result += line
 f = open('game.txt','w')
 f.write(result)
 f.close()
