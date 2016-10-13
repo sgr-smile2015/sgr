33847b32b611abb7525ba55d062f93a4bbdba04f
sgr <sgrqihai997@gmail.com>
2016-10-13 15:02:07 +0800

    change scripts re.py to testing re

--- python/class/code/day2/re.py
+++ python/class/code/day2/re.py
@@ -3,8 +3,8 @@
 import re
 #text = 'Hi,I am shirley Hilton, I am his wife'
 text = 'site sea sue sweet see case sse ssee loses'
-m = re.findall(r"\bs\S*?e\b",text)
-#m = re.findall(r"\bs.*?e\b",text)
+#m = re.findall(r"\bs\S*?e\b",text)
+m = re.findall(r"\bs.*?e\b",text)
 #m = re.findall(r"[Hh]i*",text)
 #m = re.findall(r"\bi\b",text)
 #m = re.findall(r"\b.*\S\b",text)
