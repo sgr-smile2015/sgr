22577e444fec20a246b29e8ebb4d00783dbd3947
sgr <sgrqihai997@gmail.com>
2016-10-13 14:46:48 +0800

    testing re.py text

--- python/class/code/day2/re.py
+++ python/class/code/day2/re.py
@@ -4,7 +4,7 @@ import re
 #text = 'Hi,I am shirley Hilton, I am his wife'
 text = 'site sea sue sweet see case sse ssee loses'
 m = re.findall(r"\bs\S*?e\b",text)
-#m = re.findall(r"\bs.*?e\b",text)
+m = re.findall(r"\bs.*?e\b",text)
 #m = re.findall(r"[Hh]i*",text)
 #m = re.findall(r"\bi\b",text)
 
