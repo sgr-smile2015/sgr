5a31211e2f0c85c4aa9c2ea2a13214ec24d66236
sgr <sgrqihai997@gmail.com>
2016-10-13 14:50:53 +0800

    change re.py scripts shell

--- python/class/code/day2/re.py
+++ python/class/code/day2/re.py
@@ -5,7 +5,7 @@ import re
 text = 'site sea sue sweet see case sse ssee loses'
 m = re.findall(r"\bs\S*?e\b",text)
 #m = re.findall(r"\bs.*?e\b",text)
-#m = re.findall(r"[Hh]i*",text)
+m = re.findall(r"[Hh]i*",text)
 #m = re.findall(r"\bi\b",text)
 
 if m:
