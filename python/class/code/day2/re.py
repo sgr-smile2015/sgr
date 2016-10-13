5fb1b37adfb1bd9f9da1a3f47c4b398d6ebde75e
sgr <sgrqihai997@gmail.com>
2016-10-13 14:46:05 +0800

    testing re.py text

--- python/class/code/day2/re.py
+++ python/class/code/day2/re.py
@@ -1,8 +1,8 @@
 #! /usr/bin/python
 
 import re
-text = 'Hi,I am shirley Hilton, I am his wife'
-#text = 'site sea sue sweet see case sse ssee loses'
+#text = 'Hi,I am shirley Hilton, I am his wife'
+text = 'site sea sue sweet see case sse ssee loses'
 m = re.findall(r"\bs\S*?e\b",text)
 #m = re.findall(r"\bs.*?e\b",text)
 #m = re.findall(r"[Hh]i*",text)
