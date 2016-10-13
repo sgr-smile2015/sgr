3da63b1753bf815371db986b7f9d7cea05d74ceb
sgr <sgrqihai997@gmail.com>
2016-10-13 18:14:13 +0800

    print one line in test list

--- python/class/code/day2/list.py
+++ python/class/code/day2/list.py
@@ -10,3 +10,7 @@ print list2
 list3 = [i for i in list1 if i % 2 ==0]
 print list3
 
+#list4 = [i for i in range(1,21) if i%2==0 | if i%3==0 | if i%5==0]
+print ';'.join([str(i) for i in range(1,102) if i%2==0 and i%3==0 and i%5==0])
+#print list4
+
