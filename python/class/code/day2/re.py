8d3d6c555dfc17914e3ece49861f0269602850b5
sgr <sgrqihai997@gmail.com>
2016-10-13 15:08:03 +0800

    use \d{10} find phone number

--- python/class/code/day2/re.py
+++ python/class/code/day2/re.py
@@ -2,9 +2,10 @@
 
 import re
 #text = 'Hi,I am shirley Hilton, I am his wife'
-text = 'site sea sue sweet see case sse ssee loses'
+#text = 'site sea sue sweet see case sse ssee loses'
+text = '13924102341 23456789 15807711a22 12113421 12345678900'
 #m = re.findall(r"\bs\S*?e\b",text)
-m = re.findall(r"\bs.*?e\b",text)
+m = re.findall(r"1\d{10}",text)
 #m = re.findall(r"[Hh]i*",text)
 #m = re.findall(r"\bi\b",text)
 #m = re.findall(r"\b.*\S\b",text)
