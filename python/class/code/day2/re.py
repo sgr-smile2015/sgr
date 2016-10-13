286a9d45c0be2f3fddc8cd8526e121ad7db906db
sgr <sgrqihai997@gmail.com>
2016-10-13 15:00:25 +0800

    change re.py

--- python/class/code/day2/re.py
+++ python/class/code/day2/re.py
@@ -3,11 +3,11 @@
 import re
 #text = 'Hi,I am shirley Hilton, I am his wife'
 text = 'site sea sue sweet see case sse ssee loses'
-#m = re.findall(r"\bs\S*?e\b",text)
+m = re.findall(r"\bs\S*?e\b",text)
 #m = re.findall(r"\bs.*?e\b",text)
 #m = re.findall(r"[Hh]i*",text)
 #m = re.findall(r"\bi\b",text)
-m = re.findall(r"\b.*\S\b",text)
+#m = re.findall(r"\b.*\S\b",text)
 
 if m:
     print m
