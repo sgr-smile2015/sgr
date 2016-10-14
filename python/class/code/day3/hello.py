3beca64711e7903d01b8fc77d7880144013c131c
sgr <sgrqihai997@gmail.com>
2016-10-14 10:49:34 +0800

    add file day3

---
+++ python/class/code/day3/hello.py
@@ -0,0 +1,16 @@
+# -*-coding:utf-8-*-
+import pygame
+from sys import exit
+pygame.init()
+
+screen = pygame.display.set_mode((600, 570), 0, 32)
+pygame.display.set_caption("Hello,World!")
+
+background = pygame.image.load('bg.jpg').convert()
+while True:
+    for event in pygame.event.get():
+        if event.type==pygame.QUIT:
+            pygame.quit()
+            exit()
+    screen.blit(background,(0,0))
+    pygame.display.update()
