7a7e0b806aa696cae843f75f3f7c9de967d11b61
sgr <sgrqihai997@gmail.com>
2016-10-17 15:29:59 +0800

    use scripts push github

---
+++ python/test.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+touch file
+git add file > /tmp/mygit.log 2>&1 &
+git commit -m 'testing file to system' > /tmp/mygit.log 2>&1 &
+git push > /tmp/mygit.log 2>&1 &
+
+sleep 5 
+rm ./file
+
+git rm file > /tmp/mygit.log 2>&1 &
+git commit -m 'testing file to system' > /tmp/mygit.log 2>&1 &
+git push > /tmp/mygit.log 2>&1 &
+
+
+
