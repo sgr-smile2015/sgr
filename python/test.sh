27a0b85e866c7485908ae6a3c0f858b291619852
sgr <sgrqihai997@gmail.com>
2016-10-17 15:35:16 +0800

    change test.sh scripts

--- python/test.sh
+++ python/test.sh
@@ -6,7 +6,6 @@ git commit -m 'testing file to system' > /tmp/mygit.log 2>&1 &
 git push > /tmp/mygit.log 2>&1 &
 
 sleep 5 
-rm ./file
 
 git rm file > /tmp/mygit.log 2>&1 &
 git commit -m 'testing file to system' > /tmp/mygit.log 2>&1 &
