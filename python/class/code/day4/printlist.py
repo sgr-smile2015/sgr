93aad3e42125dcca299dec4bef9f5d77fcf48d13
sgr <sgrqihai997@gmail.com>
2016-10-17 10:33:48 +0800

    printlist.py testing

--- python/class/code/day4/printlist.py
+++ python/class/code/day4/printlist.py
@@ -12,3 +12,18 @@ print "list1 = %s" % list1
 print "list2 = %s" % list2
 print "list3 = %s" % list3
 
+# output list1[10] list2[12] list3[a]
+
+def ext(val, list=None):
+    if list is None:
+        list = []
+    list.append(val)
+    return list
+
+list1 = ext(10)
+list2 = ext(12,[])
+list3 = ext('a')
+
+print "list1 = %s" % list1
+print "list2 = %s" % list2
+print "list3 = %s" % list3
