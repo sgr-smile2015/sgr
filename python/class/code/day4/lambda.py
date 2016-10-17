af212a3ee7f2e34eaeae0178adee23af59b32a71
sgr <sgrqihai997@gmail.com>
2016-10-17 10:27:48 +0800

    change bug lambda.py

--- python/class/code/day4/lambda.py
+++ python/class/code/day4/lambda.py
@@ -1,5 +1,9 @@
 #! /usr/bin/python
 
+#add for print 3
+from functools import partial
+from operator import mul
+
 def mult():
     return [lambda x :i*x for i in range(4)]
 
@@ -12,3 +16,9 @@ def mult():
 
 print [m(2) for m in mult()]
 
+#3 output list [0,2,4,6]
+
+def mult():
+    return [partial(mul, i) for i in range(4)]
+
+print [m(2) for m in mult()]
