7dc713418db88d96c6227a5ad8224a831d517d9c
sgr <sgrqihai997@gmail.com>
2016-10-18 15:47:02 +0800

    change bug to game.py

--- python/class/code/day5/game.py
+++ python/class/code/day5/game.py
@@ -2,6 +2,7 @@
 
 import pygame
 import math
+import random
 from pygame.locals import *
 
 width,height = 640, 480
@@ -9,15 +10,27 @@ width,height = 640, 480
 keys = [False, False, False, False]
 playerpos = [100,100]
 
+acc = [0,0]
+arrows = []
+
+badtimer = 100
+badtimer1 = 0
+badguys = [[640,100]]
+healthvalue = 194
+
 pygame.init()
 screen = pygame.display.set_mode((width, height))
-pygame.display.set_caption("Run bullt ^_^")
+pygame.display.set_caption("run bullt ^_^")
 
 player = pygame.image.load("resources/images/dude.png")
 grass = pygame.image.load("resources/images/grass.png")
 castle = pygame.image.load("resources/images/castle.png")
+arrow = pygame.image.load("resources/images/bullet.png")
+badguyimg1 = pygame.image.load("resources/images/badguy.png")
+badguyimg = badguyimg1
 
 while True:
+    badtimer -= 1
     screen.fill(0)
 
     for x in range(width/grass.get_width() + 1):
@@ -34,6 +47,58 @@ while True:
     playerpos1=(playerpos[0]-playerrot.get_rect().width/2,\
             playerpos[1]-playerrot.get_rect().height/2)
     screen.blit(playerrot, playerpos1)
+
+    # Draw arrows
+    for bullet in arrows:
+        index = 0
+        velx = math.cos(bullet[0])*5
+        vely = math.sin(bullet[0])*5
+        bullet[1] += velx
+        bullet[2] += vely
+        if bullet[1] < -64 or bullet[1] >640 or bullet[2] <-64 or bullet[2] > 480:
+            arrows.pop(index)
+            index += 1
+        for projectile in arrows:
+            arrow1 = pygame.transform.rotate(arrow, 360-projectile[0]*57.29)
+            screen.blit(arrow1,(projectile[1],projectile[2]))
+
+    # Draw badgers
+    if badtimer == 0:
+        badguys.append([640,random.randint(50,430)])
+        badtimer = 100 -(badtimer1)
+        if badtimer1 >= 35:
+            badtimer1 = 35
+        else:
+            badtimer1 += 5
+
+    index = 0
+    for badguy in badguys:
+        if badguy[0] < -64:
+            badguys.pop(index)
+        badguy[0] -= 7
+        index += 1
+    for badguy in badguys:
+        screen.blit(badguyimg,badguy)
+
+    # attack castle
+    badrect = pygame.Rect(badguyimg.get_rect())
+    badrect.top = badguy[1]
+    badrect.left = badguy[0]
+    if badrect.left < 64 :
+        healthvalue -= random.randint(5,20)
+        badguys.pop(index)
+
+    # check for collisions
+    index1 = 0
+    for bullet in arrows:
+        bullrect = pygame.Rect(arrow.get_rect())
+        bullrect.left = bullet[1]
+        bullrect.top = bullet[2]
+        if badrect.colliderect(bullrect):
+            acc[0] += 1
+            badguys.pop(index)
+            arrows.pop(index1)
+        index1 += 1
 
     pygame.display.flip()
 
@@ -61,6 +126,11 @@ while True:
             elif event.key == K_d:
                 keys[3] = False
 
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            position = pygame.mouse.get_pos()
+            acc[1] += 1
+            arrows.append([math.atan2(positon[1]-(playerpos1[1]+32),\
+                    position[0]-(playerpos1[0]+26)),playerpos1[0]+32,playerpos1[1]+32])
     if keys[0]:
         playerpos[1] -= 5
     elif keys[2]:
