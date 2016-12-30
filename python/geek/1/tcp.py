f700c231d65f9979b89b0f6db811e07a110cce0f
sgr <sgrqihai997@gmail.com>
2016-12-30 12:14:55 +0800

    add try except to tcp.py

--- python/geek/1/tcp.py
+++ python/geek/1/tcp.py
@@ -5,6 +5,9 @@ import socket
 socket.setdefaulttimeout(3)
 
 s = socket.socket()
-s.connect(("192.168.1.81",22))
+try:    
+    s.connect(("192.168.1.46",21))
+except Exception,e:
+    print "[-] Error = " +str(e)
 ans = s.recv(1024)
 print ans
