62ced242f719da044b28da934ea1e32088684086
sgr <sgrqihai997@gmail.com>
2017-03-14 17:00:41 +0800

    delete # in urls.py

--- python/tango_project/rango/urls.py
+++ python/tango_project/rango/urls.py
@@ -7,7 +7,7 @@ urlpatterns = patterns('',
         )
 
 #from rango import views
-#
+
 #urlpatterns = patterns('',
 #        url(r'^$' , views.index , name='index'),
 #        url(r'^about/', views.about , name='about')
