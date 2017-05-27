98590fb8b247358566b6d5e9d89fd64acca66379
sgr <sgrqihai997@gmail.com>
2017-05-27 15:55:00 +0800

    修正bak.py读取mongod数据库

--- python/sendMail/bak.py
+++ python/sendMail/bak.py
@@ -3,13 +3,49 @@
 
 import psycopg2
 import psycopg2.extras
+import pymongo
 
 from get_user_pay import get_timestamp
 
+#mongo_uri = "mongodb://ipin/mongo1,mongo2/wmzy"
+
+def query_mongo(user_id):
+    ret = []
+    #查询失败,返回错误
+    err_data = [[u'unknown', 'unknown', 'unknown', u'unknown', 'unknown', 'unknown']]
+
+    conn = pymongo.MongoClient('mongo2')
+    db = conn.wmzy
+    cur = db.user_score_info.find_one(
+                                       {"user_id":user_id}, 
+                                       ["user_id", "score_rank", "score_type", "province_id", "score", "wenli"]
+                                      )
+    #cur.pop('_id')
+    if cur == None:
+        return err_data
+
+    if 'province_id' in cur.keys():
+        ret.append(cur['province_id'])
+    if 'wenli' in cur.keys():
+        ret.append(cur['wenli'])
+    if 'score' in cur.keys():
+        ret.append(cur['score'])
+    if 'score_type' in cur.keys():
+        ret.append(cur['score_type'])
+    if 'score_rank' in cur.keys():
+        ret.append(cur['score_rank'])
+    if 'user_id' in cur.keys():
+        ret.append(cur['user_id'])
+
+    conn.close()
+    des = []
+    des.append(ret) #转换数据格式[[a, b]]
+    return des
+
 
 def query(table, condition, start, end, lable, other=''):
-    conn = psycopg2.connect(database="wmzy",user="app_wmzy" , password="7qP4",host="192.168.1.45") 
-    #conn = psycopg2.connect(database="wmzy", user="opsapp", password="pZtv6iYgG", host="10.10.13.86")
+    #conn = psycopg2.connect(database="wmzy",user="app_wmzy" , password="fseqP44QZfc6nci",host="192.168.1.45") 
+    conn = psycopg2.connect(database="wmzy", user="opsapp", password="tF6QiiMcxnJv6iYgG", host="10.27.113.186") 
     cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
     #cur = conn.cursor()
     cur.execute("select count(*) from %s where %s between %d and %d group by %s %s" % (table, condition, start, end, lable, other))
@@ -18,8 +54,8 @@ def query(table, condition, start, end, lable, other=''):
     return ret
 
 def query_pay(table, condition, start, end, lable):
-    conn = psycopg2.connect(database="wmzy",user="app_wmzy" , password="70yLi",host="192.168.1.45") 
-    #conn = psycopg2.connect(database="wmzy", user="opsapp", password="pZtv6iYgG", host="10.10.13.86")
+    #conn = psycopg2.connect(database="wmzy",user="app_wmzy" , password="fseqP44QZfc6nci",host="192.168.1.45") 
+    conn = psycopg2.connect(database="wmzy", user="opsapp", password="tF6QiiMcxnJv6iYgG", host="10.27.113.186") 
     cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
     cur.execute("select user_id, order_time, order_type, buy_platform, buy_channel, \
                  coupon_code, pay_count \
@@ -29,10 +65,10 @@ def query_pay(table, condition, start, end, lable):
     return ret
 
 def query_user(table, condition, user_id):
-    conn = psycopg2.connect(database="wmzy",user="app_wmzy" , password="7d0bOfseqPnci",host="192.168.1.45") 
-    #conn = psycopg2.connect(database="wmzy", user="opsapp", password="pZtv6iYgG", host="10.10.13.86")
+    #conn = psycopg2.connect(database="wmzy",user="app_wmzy" , password="fse6nci",host="192.168.1.45") 
+    conn = psycopg2.connect(database="wmzy", user="opsapp", password="BgG", host="10.27.113.186")
     cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
-    cur.execute("select user_id, register_platform, register_channel, \
+    cur.execute("select register_platform, register_channel, \
                  bound_trial_card, ts_create, status \
                  from %s where %s = %s" % (table, condition, user_id))
     ret = cur.fetchall()
@@ -41,10 +77,7 @@ def query_user(table, condition, user_id):
 
 if __name__ == "__main__":
     #ret = query_pay('user_package_order','order_time', 1477929600, 1485878400, 'pay_status=1 limit 4')
-    ret = query_user('user_account','user_id', 10004162)
-    #ret = query('user_account','ts_create', 1477929600, 1485878400,'register_channel','')
-    #ret = query('card_info','active_time', 1490976000, 1492569426,'card_type',[2])
-#    ret = query_pg(0, get_timestamp("2017-03-12 00:00:00"), [4])
+    #ret = query_user('user_account','user_id', 2348905)
+    ret = query_mongo(20151722)
     print ret
-    #print ret[1]
 
