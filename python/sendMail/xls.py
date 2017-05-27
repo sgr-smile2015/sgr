3630d36fed0f42aa0628e3e3ccaa38d83c65e4c3
sgr <sgrqihai997@gmail.com>
2017-05-27 15:52:53 +0800

    修改部分可读取mongodb数据

--- python/sendMail/xls.py
+++ python/sendMail/xls.py
@@ -2,12 +2,47 @@
 #_*_ coding:utf-8 _*_
 
 import xlwt
-import json
 import os
 import time
+import datetime
 
 from bak import query_pay as query_pay
 from bak import query_user as query_user
+from bak import query_mongo as query_mongo
+
+pronvice_dic = {
+"360000000000":"江西",
+"440000000000":"广东",
+"370000000000":"山东",
+"350000000000":"福建",
+"610000000000":"陕西",
+"520000000000":"贵州",
+"110000000000":"北京",
+"430000000000":"湖南",
+"140000000000":"山西",
+"420000000000":"湖北",
+"130000000000":"河北",
+"320000000000":"江苏",
+"410000000000":"河南",
+"510000000000":"四川",
+"150000000000":"内蒙古",
+"530000000000":"云南",
+"500000000000":"重庆",
+"450000000000":"广西",
+"230000000000":"黑龙江",
+"340000000000":"安徽",
+"460000000000":"海南",
+"650000000000":"新疆",
+"210000000000":"辽宁",
+"220000000000":"吉林",
+"120000000000":"天津",
+"620000000000":"甘肃",
+"330000000000":"浙江",
+"640000000000":"宁夏",
+"540000000000":"西藏",
+"630000000000":"青海",
+"310000000000":"上海",
+            }
 
 
 topic = [
@@ -17,21 +52,52 @@ topic = [
         '购买的渠道',
         '订单来源渠道',
         '优惠码',
-        '金额(分)',
-        '用户ID',
+        '金额(元)',
         '注册类型',
         '注册平台',
         '体验卡次数',
         '创建时间',
-        '有效状态'
+        '有效状态',
+        '省份',
+        '文理',
+        '分数',
+        'score_type',
+        'score_rank',
+        'id'
         ]
 
 
-def get_timestamp():
+def ret_pronvice_id(key):
+    if key in pronvice_dic.keys():
+        return pronvice_dic[key]
+    else:
+        return 'unknown'
+
+def ret_wenli(num):
+    if num == 1:
+        return '文'
+    if num == 2:
+        return '理'
+    if num == 0:
+        return '综合'
+    else:
+        return 'unknown'
+
+def ret_week_time():
+    d = datetime.datetime.now()
+    week = d.weekday()
+    if week == 5:
+        return True
+    else:
+        return False
+
+
+
+def get_timestamp(days):
     t = time.localtime()
     day = int(time.mktime(t))
     t2 = list(t)
-    t2[2] = t2[2] - 1
+    t2[2] = t2[2] - days
     yesterday = int(time.mktime(t2)) 
     
     return yesterday, day
@@ -110,46 +176,65 @@ def remove_dir_file(t_dir):
 def init_data(user_pay_list):
     user_id_list = []
     user_list = []
+    score_list = []
     size_list = len(user_pay_list)
     
     for i in range(size_list):
             user_id_list.append(user_pay_list[i][0]) #获取用ID列表
     
+    #print user_id_list
     for id_num in user_id_list:
         one_user = query_user('user_account', 'user_id', id_num)
         user_list.extend(one_user)
+        score_data = query_mongo(id_num)
+        #time.sleep(0.5)
+        score_list.extend(score_data)
     
     for t in range(size_list):
         user_pay_list[t].extend(user_list[t])
+        user_pay_list[t].extend(score_list[t])
+    
 
     for i in range(size_list):
             user_pay_list[i][1] = ret_local_time(user_pay_list[i][1]) #转换订单时间
             user_pay_list[i][3] = ret_buy_platform(user_pay_list[i][3])
-            user_pay_list[i][8] = ret_register_platform(user_pay_list[i][8])
-            user_pay_list[i][9] = ret_register_channel(user_pay_list[i][9])
-            user_pay_list[i][11] = ret_local_time(user_pay_list[i][11])
+            user_pay_list[i][7] = ret_register_platform(user_pay_list[i][7])
+            user_pay_list[i][8] = ret_register_channel(user_pay_list[i][8])
+            user_pay_list[i][10] = ret_local_time(user_pay_list[i][10]/1000)
+            user_pay_list[i][12] = ret_pronvice_id(user_pay_list[i][12])#省份转换
+            user_pay_list[i][13] = ret_wenli(user_pay_list[i][13])#文理
+
     
-    #print user_pay_list
+#    print user_pay_list
     return user_pay_list
 
 
-def save2excel(excel_name, data):
+def save2excel(excel_name, data, weekdata):
     wb = xlwt.Workbook(encoding = 'utf-8', style_compression=0)
     sheet = wb.add_sheet("总体日报表", cell_overwrite_ok=True)
+    sheet_week = wb.add_sheet("每周报表", cell_overwrite_ok=True)
     #style = xlwt.easyxf('font: bold on')
     style_top = xlwt.easyxf('font: bold on; align: wrap on, vert centre, horiz center')
     style_mul = xlwt.easyxf('font: bold no; align: wrap on')
     for i in range(len(topic)):
         sheet.write(0, i, topic[i], style_top)
-        for j in range(len(data)):    
+        for j in range(len(data)):
             sheet.write(j+1, i, data[j][i], style_mul)
+    if ret_week_time():
+        for x in range(len(topic)):
+            sheet_week.write(0, x, topic[x], style_top)
+            for y in range(len(weekdata)):
+                sheet_week.write(y+1, x, weekdata[y][x], style_mul)
     wb.save(excel_name)
 
 
 if __name__ == '__main__':
-    yesterday , day = get_timestamp()
+    yesterday , day = get_timestamp(1)
+    weekago , day = get_timestamp(7)
     remove_dir_file('./excel')
-    data = query_pay('user_package_order','order_time', yesterday, day, 'pay_status=1 limit 3')
+    data = query_pay('user_package_order','order_time', yesterday, day, 'pay_status=1')
+    weekdata = query_pay('user_package_order','order_time', weekago, day, 'pay_status=1')
     init_data(data)
-    save2excel("./excel/excel.xls", data)
+    init_data(weekdata)
+    save2excel("./excel/excel.xls", data, weekdata)
     rename_file('./excel', day)
