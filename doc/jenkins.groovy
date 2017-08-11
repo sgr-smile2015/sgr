d8a2a0bd083cf1a5aa0d49fe55926217761f94d3
sgr <sgrqihai997@gmail.com>
2017-08-11 12:01:06 +0800

    添加jenkins.groovy注释"

--- doc/jenkins.groovy
+++ doc/jenkins.groovy
@@ -1,5 +1,8 @@
 #!/usr/bin/groovy
 
+//所用到的jenkins插件是Extended Choice Parameter Plug-In
+//该脚本是获取当前的进程的job名称,使用job名称查找对应的docker
+//images tags
 import groovy.json.JsonSlurper 
 
 //get jenkins job_name
@@ -9,7 +12,8 @@ def match = build  =~ regexp
 def job_name = match[0][1]
 
 //get docker images tags
-def  url = 'http://sgr0.docker.com/api/repositories/common/java/tags'
+//def  url = 'http://sgr0.docker.com/api/repositories/common/java/tags'
+def  url = 'http://sgr0.docker.com/api/repositories/' + job_name + '/java/tags'
 def tags = url.toURL().text
 def s = new JsonSlurper()
 def object = s.parseText(tags)
